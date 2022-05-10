6ba7691663c6e9687a634edc59dd3f0b96f01a92
kajela <titoyut56@gmail.com>
2022-05-10 06:25:54 -0400

    Initial commit

---
+++ src/TITO.java
@@ -0,0 +1,13 @@
+abstract class TITO {
+    public String born = "gatundu south";
+    public String school = "muhoho high school";
+    public int age = 22;
+
+    public abstract void proffesion();//main method.
+}
+     class kajela extends TITO{
+    public void proffesion() {
+        System.out.println("i really want to be the best in IT");
+
+     }
+}
---
+++ src/second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args) {
+        kajela myObj = new kajela();
+        System.out.println("i was born in: "+myObj.born);
+        System.out.println("i went to: "+myObj.school);
+        System.out.println("my age is: "+myObj.age);
+        myObj.proffesion();
+
+    }
+}
